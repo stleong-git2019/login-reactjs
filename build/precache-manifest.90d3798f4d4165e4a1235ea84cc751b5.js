4e2b0796265c4c0074db7bc6ccf29d6c38c5838b
stleong-git2019 <57035456+stleong-git2019@users.noreply.github.com>
2019-10-26 21:06:37 +0800

    Add files via upload

---
+++ build/precache-manifest.90d3798f4d4165e4a1235ea84cc751b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5348710daaebe630d840375a8709405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f960e4bb5a2adfcf85b",
+    "url": "/static/css/main.cb295984.chunk.css"
+  },
+  {
+    "revision": "e8cf17851bc84b734472",
+    "url": "/static/js/2.c238d951.chunk.js"
+  },
+  {
+    "revision": "8f960e4bb5a2adfcf85b",
+    "url": "/static/js/main.bb6c450e.chunk.js"
+  },
+  {
+    "revision": "f0a1ad96e7fa4604824d",
+    "url": "/static/js/runtime-main.8f5c2c68.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
